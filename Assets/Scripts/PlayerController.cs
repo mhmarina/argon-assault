b16d87bf9e2fa63956731cca26da3cd0e797e858
mhmarina <marinamagedsamuel@gmail.com>
2023-08-22 02:10:41 -0700

    Made movement nifty by adding rotations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,24 @@ public class PlayerController : MonoBehaviour
     public float controlSpeed = 25f;
     public float xRange = 10f;
     public float yRange = 5f;
+    public float posPitchFactor = -2f;
+    public float controlPitchFactor = -10f;
+    public float posYawFactor = -5f;
+    public float controlRollFactor = 5f;
 
-    void Start()
-    {
-        
-    }
+    float yThrow;
+    float xThrow;
 
     void Update()
     {
-        float xThrow =  Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float newX = transform.localPosition.x + xOffset;
@@ -26,8 +34,15 @@ public class PlayerController : MonoBehaviour
         float newY = transform.localPosition.y + yOffset;
         float clampedY = Mathf.Clamp(newY, -yRange, yRange);
 
-        transform.localPosition = new Vector3(clampedX, 
+        transform.localPosition = new Vector3(clampedX,
                                               clampedY,
                                               transform.localPosition.z);
+    }
+
+    private void ProcessRotation() {
+        float pitch = transform.localPosition.y * posPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * posYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); 
     }
 }
